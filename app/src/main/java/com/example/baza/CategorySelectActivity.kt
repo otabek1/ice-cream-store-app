70100fd0441fed45ea955c03383d7e3e4bd5164c
Otabek <abduraufov_otabek@mail.ru>
2021-02-15 01:22:46 +0500

    add category, finish sign up

---
+++ app/src/main/java/com/example/baza/CategorySelectActivity.kt
@@ -0,0 +1,94 @@
+package com.example.baza
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_category_select.*
+
+
+class CategorySelectActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    val TAG: String = "nima"
+    val db = Firebase.firestore
+    val names = arrayListOf<String>()
+    lateinit var radioButton: RadioButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_select)
+
+        auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
+        db.collection("categories")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    names.add(document["name"] as String)
+
+                    Log.d(TAG, "sdas " + document["name"])
+                }
+                createRadioButton()
+                setSelectBtn()
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+
+
+    }
+
+    private fun setSelectBtn() {
+
+        selectCategoryBtn.setOnClickListener {
+
+            if (radioGroup1.checkedRadioButtonId == -1) {
+                // no radio buttons are checked
+                Toast.makeText(
+                    baseContext,
+                    getString(R.string.kategoriyani_tanlang),
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            } else {
+                // one of the radio buttons is checked
+                val selectedOption: Int = radioGroup1!!.checkedRadioButtonId
+
+                // Assigning id of the checked radio button
+                radioButton = findViewById(selectedOption)
+
+                val intent = Intent(this, RegisterActivity::class.java)
+                intent.putExtra("category", radioButton.text)
+                startActivity(intent)
+                finish()
+
+                // Displaying text of the checked radio button in the form of toast
+                Toast.makeText(baseContext, radioButton.text, Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+    }
+
+
+    private fun createRadioButton() {
+        val rb = arrayOfNulls<RadioButton>(names.size)
+
+        for (i in 0..rb.size - 1) {
+            rb[i] = RadioButton(this)
+            rb[i]?.setTextSize(18f)
+            radioGroup1.addView(rb[i]) //the RadioButtons are added to the radioGroup instead of the layout
+            rb[i]!!.text = names[i]
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/baza/RegisterActivity.kt
@@ -0,0 +1,111 @@
+package com.example.baza
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    // Access a Cloud Firestore instance from your Activity
+    val db = Firebase.firestore
+    val TAG: String = "nima"
+    var category: String = ""
+    val user = hashMapOf(
+        "userId" to "",
+        "email" to "",
+        "category" to ""
+    )
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        category = getIntent().getStringExtra("category").toString()
+
+        auth = FirebaseAuth.getInstance()
+
+        loginBtn.setOnClickListener {
+
+        }
+
+
+        registerBtn.setOnClickListener {
+            signUpUser()
+        }
+        Log.d(TAG, "onCreate: ")
+
+        base_constraint.setOnClickListener {
+            hideKeyboard(base_constraint)
+        }
+    }
+
+    private fun signUpUser() {
+        if (emailEditText.text.toString().isEmpty()) {
+            emailEditText.error = getString(R.string.email_kirit)
+            emailEditText.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailEditText.text.toString()).matches()) {
+            emailEditText.error = getString(R.string.togri_email_kirit)
+            emailEditText.requestFocus()
+            return
+        }
+
+        if (passwordEditText.text.toString().isEmpty()) {
+            passwordEditText.error = getString(R.string.parol_kirit)
+            passwordEditText.requestFocus()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(
+            emailEditText.text.toString(),
+            passwordEditText.text.toString()
+        )
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val current_user = auth.currentUser
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    user["userId"] = current_user?.uid.toString();
+                    user["email"] = current_user?.email.toString()
+                    user["category"] = category.toString()
+                    db.collection("users").add(user)
+                    val mainIntent = Intent(this, MainActivity::class.java)
+                    startActivity(mainIntent)
+
+//                        updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed. " + task.exception?.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+//                        updateUI(null)
+                }
+
+                // ...
+            }
+
+    }
+
+    fun Context.hideKeyboard(view: View) {
+        val inputMethodManager =
+            getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+}
